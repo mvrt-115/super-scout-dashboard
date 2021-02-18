27a0164180ee453ac9237466993a7b6c8055c794
sohan-shingade <sohanshingade@gmail.com>
2021-02-18 14:43:18 -0800

    changed scanner redirect to website

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+  import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Home from './pages/Home';
@@ -11,7 +11,10 @@ function App() {
     <Router>
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route exact path="/scanner" component={Scanner} />
+        <Route exact path="/scanner" component={() => { 
+            window.location.href = 'https://mvrt-super-scout-scanner.web.app/'; 
+            return null;
+        }} />
         <Route exact path="/regional/:regional" component={Regional} />
         <Route exact path="/regional/:regional/match/:match" component={Match} />
       </Switch>
